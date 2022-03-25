1fec980d7586620714411358a13ff6bcf2892ab0
mowuj <mowuj92@gmail.com>
2022-03-25 22:18:35 +0600

    initialize

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>Pick Your Phone</h1>
+            <div className='shop-container'>
+                <h2>This is Shop Container</h2>
+            </div>
+            <div className='cart-container'>
+                <h2>This is Cart container</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
